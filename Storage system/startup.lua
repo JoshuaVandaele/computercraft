752b1722d1262d6d8431d10e5839ad256a9590e0
FolfyBlue <folfyblu@gmail.com>
2020-12-29 01:49:15 +0100

    Initial commit

---
+++ Storage system/startup.lua
@@ -0,0 +1,32 @@
+local deposit_chest = "quark:quark_chest_1630"
+ 
+local mon = peripheral.wrap("top")
+local storage = {}
+local i = 0
+ 
+print("Searching for chests..")
+for k,v in pairs(peripheral.getNames()) do
+  if v:match("chest") and not v:match(deposit_chest) then
+    print("Found "..v)
+    storage[#storage+1] = peripheral.wrap(v)
+  end
+end
+
+print("Starting.")
+while true do
+  sleep(1)
+  items = {}
+
+  for _,chest in pairs(storage) do
+    for slot = 1,#chest.list() do
+      for _,item in pairs(chest.getItemMeta(slot)) do
+        items[item.displayName] = (items[item.displayName] or 0) + (item.count or 0)
+      end
+    end
+  end
+
+  for k,v in pairs(items) do
+    print(k,v)
+  end
+
+end
