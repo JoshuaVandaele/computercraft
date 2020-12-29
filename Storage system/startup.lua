56008b14d50489859e345d4bfa642ddaa8c49f99
Joshua Vandaële <joshua@vandaele.software>
2020-12-29 02:59:55 +0100

    Monitor

--- Storage system/startup.lua
+++ Storage system/startup.lua
@@ -1,41 +1,52 @@
 local deposit_chest = "quark:quark_chest_1630"
- 
+  
 local mon = peripheral.wrap("top")
 local storage = {}
 local i = 0
- 
-print("Searching for chests..")
-for k,v in pairs(peripheral.getNames()) do
-	if v:match("chest") and not v:match(deposit_chest) then
-		print("Found "..v)
-		storage[#storage+1] = peripheral.wrap(v)
-	end
+
+local width,height = term.getSize()
+local itemDisplay = window.create(mon.current(),3,1,width,height-6)
+
+function seekChests()
+    storage = {}
+    for k,v in pairs(peripheral.getNames()) do
+        if v:match("chest") and not v:match(deposit_chest) then
+            storage[#storage+1] = peripheral.wrap(v)
+        end
+    end
 end
 
-print("Starting.")
-while true do
-	sleep(1)
-	items = {}
+function getItems()
+    local items = {}
+    for _,chest in pairs(storage) do
+        for slot = 1,chest.size() do
+            item = chest.getItemMeta(slot)
+            if item ~= nil then
+                items[item.displayName] = { 
+                    ["count"] = (items[item.displayName] or 0) + (item.count or 0),
+                    ["maxDamage"] = item.maxDamage,
+                }
+ 
+                if items[item.displayName]["damage"] then
+                    table.insert(items[item.displayName]["damage"],item.damage)
+                else
+                    items[item.displayName]["damage"] = {item.damage}
+                end
+            end
+        end
+    end
+    return items
+end
 
-	for _,chest in pairs(storage) do
-		for slot = 1,chest.size() do
-			item = chest.getItemMeta(slot)
-			if item ~= nil then
-				items[item.displayName] = { 
-					["count"] = (items[item.displayName] or 0) + (item.count or 0),
-					["maxDamage"] = item.maxDamage,
-				}
+seekChests()
 
-				if items[item.displayName]["damage"] then
-					table.insert(items[item.displayName]["damage"],item.damage)
-				else
-					items[item.displayName]["damage"] = {item.damage}
-				end
-			end
-		end
-	end
+items = getItems()
 
-	for k,v in pairs(items) do
-		print(k,v)
-	end
+mon.setCursorPos(1,startPos)
+
+for k,v in pairs(items) do
+        local x,y = mon.getCursorPos()
+        mon.setCursorPos(1,y+1)
+        mon.clearLine()
+        mon.write(v.count.." "..k.."\n")
 end
