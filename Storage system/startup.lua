c0d3b7d7d93fe2ce6d723531b69b06185774fc35
FolfyBlue <folfyblu@gmail.com>
2020-12-29 02:32:55 +0100

    Fix

--- Storage system/startup.lua
+++ Storage system/startup.lua
@@ -6,36 +6,36 @@ local i = 0
  
 print("Searching for chests..")
 for k,v in pairs(peripheral.getNames()) do
-  if v:match("chest") and not v:match(deposit_chest) then
-    print("Found "..v)
-    storage[#storage+1] = peripheral.wrap(v)
-  end
+	if v:match("chest") and not v:match(deposit_chest) then
+		print("Found "..v)
+		storage[#storage+1] = peripheral.wrap(v)
+	end
 end
 
 print("Starting.")
 while true do
-  sleep(1)
-  items = {}
+	sleep(1)
+	items = {}
 
-  for _,chest in pairs(storage) do
-    for slot = 1,chest.size() do
-      for _,item in pairs(chest.getItemMeta(slot)) do
-        if type(item) == "table" then
-          items[item.displayName] = { 
-            ["count"] = (items[item.displayName] or 0) + (item.count or 0),
-            ["maxDamage"] = item.maxDamage,
-          }
-          if items[item.displayName]["damage"] then
-            table.insert(items[item.displayName]["damage"],item.damage)
-          else
-            items[item.displayName]["damage"] = {item.damage}
-          end
-        end
-      end
-    end
-  end
+	for _,chest in pairs(storage) do
+		for slot = 1,chest.size() do
+			item = chest.getItemMeta(slot)
+			if item ~= nil then
+				items[item.displayName] = { 
+					["count"] = (items[item.displayName] or 0) + (item.count or 0),
+					["maxDamage"] = item.maxDamage,
+				}
 
-  for k,v in pairs(items) do
-    print(k,v)
-  end
+				if items[item.displayName]["damage"] then
+					table.insert(items[item.displayName]["damage"],item.damage)
+				else
+					items[item.displayName]["damage"] = {item.damage}
+				end
+			end
+		end
+	end
+
+	for k,v in pairs(items) do
+		print(k,v)
+	end
 end
