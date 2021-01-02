2a50de1b0b3b55f0a746fc80b73b0a57c850ca2c
FolfyBlue <folfyblu@gmail.com>
2021-01-02 20:06:34 +0100

    Optimisation

--- Neural Interface/miner.lua
+++ Neural Interface/miner.lua
@@ -2,28 +2,32 @@ local ni = peripheral.wrap("back")
 local trigger = "minecraft:wooden_pickaxe"
 local targets = {...}
 
+function table_invert(t)
+   local s={}
+   for k,v in pairs(t) do
+     s[v]=k
+   end
+   return s
+end
+
+targets = table_invert(targets)
+
 while true do
   holding = ni.getEquipment().list()[1]
   if holding and holding.name == trigger or trigger == "" then
   	local yaw,pitch
   	if #targets>0 then
+
   		local blocks = ni.scan()
   		local stop = false
+
   		for _,block in pairs(blocks) do
-  			for _,target in pairs(targets) do
-  				if block["name"] == target then
-  					local x, y, z = block.x, block.y, block.z
-  					yaw = math.deg(math.atan2(-x, z))
-  					pitch = math.deg(-math.atan2(y, math.sqrt(x * x + z * z)))
-  					stop = true
-  					break
-  				end
-  				if stop then
-  					break
-  				end
-  			end
-  			if stop then
-  				break
+  			if targets(block.name) then
+  				local x, y, z = block.x, block.y, block.z
+				yaw = math.deg(math.atan2(-x, z))
+				pitch = math.deg(-math.atan2(y, math.sqrt(x * x + z * z)))
+				stop = true
+				break
   			end
   		end
   	else
