58a30eaa6b32f66df2044d815c86e01bfa73c7f9
Joshua Vandaële <joshua@vandaele.software>
2021-01-03 08:18:44 +0100

    test

--- File storage/makeBlocks.lua
+++ File storage/makeBlocks.lua
@@ -1,5 +1,5 @@
 drives = {}
-blocksize = 200 -- blocksize, in mb
+blocksize = 200 -- blocksize, in b
 
 local function serialize(data, name)  -- Store data
     if not fs.exists('/.data/') then
@@ -12,16 +12,19 @@ end
 
 local function seekDrives() 
     drives = {}
-    for k, v in pairs(peripheral.getNames()) do
+    local peri = peripheral.getNames()
+    for k, v in pairs(peri) do
         if v:match("drive") then
             local per = peripheral.wrap(v)
             local path = per.getMountPath()
             if per.hasData() then
                 blocks = {}
                 for i = 1,oldfs.getCapacity()/blocksize do
-                    blocks[i] = ""
+                    blocks[i] = {}
                 end
-                drives[#drives + 1] = {["peripheral"] = per , ["n"] = path, ["blocks"] = {}}
+                drives[#drives + 1] = {["n"] = path, ["blocks"] = blocks}
+                print("Found "..#drives.." drives.")
+                sleep(0.2)
             end
         end
     end
