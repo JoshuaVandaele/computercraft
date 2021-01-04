04e81da3e0f63ab84266bb968b1c290cf01d150b
Joshua Vandaële <joshua@vandaele.software>
2021-01-04 18:50:32 +0100

    yeet

--- AutoKFC/crafter.lua
+++ AutoKFC/crafter.lua
@@ -5,38 +5,41 @@ local craftSlot = {1,2,3,5,6,7,9,10,11}
 local storage,storageinvSlots,trash,crafts = dofile("disk/storage.lua")
 local storageinv = peripheral.wrap(storage)
 local trash = peripheral.wrap(trash)
+local selfinv = peripheral.wrap("right")
 
-local function scanInventory()
-	local inventory = {}
-	for i = 1,16 do
-		inventory[i] = turtle.getItemDetail(i)
+if not selfinv or not selfinv.hasModule or not selfinv.hasModule("plethora:introspection") then
+	print("Introspection module not found, using fallback function (Might be laggier)")
+	selfinv = {}
+	selfinv.getInventory = function ()-- Not used, I have the module, can't be source of lag
+		return {                      -- Also it's ugly, soz
+			["list"] =	function()	
+							local inventory = {}
+							for i = 1,16 do
+								inventory[i] = turtle.getItemDetail(i)
+							end
+							return inventory
+						end
+		}
 	end
-	return inventory
 end
 
-local function storeItems()
-	local stored = scanInventory()
+local function storeItems()  -- main (?) source of peripheral calls, optimisation must be high
+	local stored = selfinv.getInventory().list()
 	for slot,item in pairs(stored) do
 		if storageinvSlots[item.name] then
 			storageinv.pullItems(self,slot,64,storageinvSlots[item.name])
+		else
+			print("Trashing "..item.name)
+			trash.pullItems(self,slot,64)
 		end
-	end
-	local stored = scanInventory()
-	for _,_ in pairs(stored) do -- I hate this, but #stored doesn't work because lua's stupid
-		print("Stocking overflowed items")
-		for i = 1,16 do
-			trash.pullItems(self,i,64)
-		end
-		break
 	end
 end
 
 local function make(craft,stored)
 	if not stored then stored = storageinv.list() end
-	if not stored or not craft then return end
+	if not craft then return end
 	if stored[ storageinvSlots[ craft["output"] ] ] and stored[ storageinvSlots[ craft["output"] ] ].count == 64 then
 		print("We already have too much of this item, aborting")
-		storeItems()
 		return
 	end
 
@@ -83,7 +86,7 @@ local function make(craft,stored)
 	local pulled = 0
 	print("Crafting "..craft["output"].."..")
 	for slot, item in ipairs(craft) do
-		pulled = storageinv.pushItems(self,storageinvSlots[item],1,craftSlot[slot])
+		pulled = storageinv.pushItems(self,storageinvSlots[item],1,craftSlot[slot])  -- Might also be a cause of lag, trying to make the less calls here
 		if pulled == 0 then
 			print("Something wrong happened, aborting!")
 			storeItems()
@@ -97,7 +100,7 @@ local function make(craft,stored)
 	return true
 end
 
-local function sortChest()
+local function sortChest()  -- only used once, optimisation urgency is low
 	local count = 1
 	while count ~= 0 do
 		count = 0
--- AutoKFC/furnace.lua
+++ AutoKFC/furnace.lua
@@ -47,6 +47,16 @@ local function storeItems()
 	end
 end
 
+local function emptyFurnaces()
+	for i = 1,#furnaces do
+		furnaces[i].pushItems(self,1,64)
+		furnaces[i].pushItems(self,2,64)
+		furnaces[i].pushItems(self,3,64)
+	end
+end
+
+emptyFurnaces()
+
 while true do
 	storeItems()
 	local work = 0
@@ -65,20 +75,24 @@ while true do
 		end
 	end
 	if work > 0 then
-		fuel.pushItems(self,1,math.ceil(math.ceil(work/4)/1.5))
-		turtle.select(16)
-		turtle.craft()
-		turtle.select(1)
-		for _ = 1,math.ceil(work/#furnaces) do
-			for i in pairs(working) do
-				furnaces[i].pullItems(self,16,1,2)
+		local fuelAmnt = math.ceil(math.ceil(work/4)/1.5)
+		local received = fuel.pushItems(self,1,fuelAmnt)
+		if received < fuelAmnt then
+			print("Not enough wood, aborting!")
+			sleep(60)
+		else
+			turtle.select(16)
+			turtle.craft()
+			turtle.select(1)
+			for _ = 1,math.ceil(work/#furnaces) do
+				for i in pairs(working) do
+					furnaces[i].pullItems(self,16,1,2)
+				end
 			end
+			print("Waiting "..(10.1*math.ceil(work/#furnaces)).." seconds for the items to process..")
+			sleep(10.1*math.ceil(work/#furnaces))
 		end
-		print("Waiting "..(10.1*math.ceil(work/#furnaces)).." seconds for the items to process..")
-		sleep(10.1*math.ceil(work/#furnaces))
-		for i = 1,#furnaces do
-			furnaces[i].pushItems(self,3,64)
-		end
+		emptyFurnaces()
 		storeItems()
 	end
 	sleep(60)
