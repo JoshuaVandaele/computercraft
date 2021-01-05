95abf9c3c43ad23dc6863ddefd8787a9a6f0d414
Joshua Vandaële <joshua@vandaele.software>
2021-01-05 11:02:19 +0100

    Forgot to remove debug messages

--- AutoKFC/milk.lua
+++ AutoKFC/milk.lua
@@ -8,10 +8,8 @@ while true do
 	local stored = nil
  	local item = turtle.getItemDetail() or {}
  	if not item.name then 
- 		print("No item name")
  		stored = storage.list()
 		if not stored[ storageSlots["minecraft:milk_bucket"] ] then
-			print("No milk")
 		    storage.pushItems(self,storageSlots["minecraft:bucket"],1,1)
 		    item.name = "minecraft:bucket"
 	 	end	
