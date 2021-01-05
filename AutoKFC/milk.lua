8c395f252487164685871dc3c3b01d3cde018452
Joshua Vandaële <joshua@vandaele.software>
2021-01-05 10:56:51 +0100

    HUGE improvement

--- AutoKFC/milk.lua
+++ AutoKFC/milk.lua
@@ -3,20 +3,32 @@ self = "turtle_3767"
 local storage,storageSlots = dofile("disk/storage.lua")
 storage = peripheral.wrap(storage)
 
+
 while true do
-  	stored = storage.list()
-	
-	if not stored[ storageSlots["minecraft:milk_bucket"] ] and not turtle.getItemDetail() then
-	    sleep(0.5)
-	    storage.pushItems(self,storageSlots["minecraft:bucket"],1,1)
+	local stored = nil
+ 	local item = turtle.getItemDetail() or {}
+ 	if not item then 
+ 		stored = storage.list()
+		if not stored[ storageSlots["minecraft:milk_bucket"] ] and not item then
+		    storage.pushItems(self,storageSlots["minecraft:bucket"],1,1)
+		    item.name = "minecraft:bucket"
+	 	end	
  	end
 
- 	if turtle.getItemDetail() then
-	 	if turtle.getItemDetail().name == "minecraft:bucket" then
-	    turtle.place()
-	 	end
-	 	if turtle.getItemDetail().name == "minecraft:milk_bucket" then
-		    storage.pullItems(self,1,1,storageSlots["minecraft:milk_bucket"])
-	 	end
-	 end
+ 	if item.name == "minecraft:bucket" then
+	    if not turtle.place() then
+	    	print("Something went wrong, sleeping to avoid server lag!")
+	    	sleep(120)
+	    else
+	    	item.name = "minecraft:milk_bucket"
+	    end
+	end
+
+	if item.name == "minecraft:milk_bucket" then
+	    storage.pullItems(self,1,1,storageSlots["minecraft:milk_bucket"])
+	  	sleep(1)
+	else
+	   	sleep(10)
+	end
 end
+
